2746bca2f683c10e86c363b43c0d599bd449198e
Caiden <98279099+PawsPixeled@users.noreply.github.com>
2024-09-05 16:56:12 -0500

    Add files via upload

---
+++ Program_1.py
@@ -0,0 +1,3 @@
+#Programmer Caiden Nelson
+#Created on 9/05/24
+rint('Hello Wolrd')
---
+++ Program_2.py
@@ -0,0 +1,7 @@
+#Programmer Caiden Nelson
+#Created on 9/05/24
+print('''Do you see a man in his skillful work?
+
+He will stand before kings;
+
+He will not stand before obscure men.''')
